ad0a0d91a44611882b4460d3f4dbbda0a3ef3aaa
kiran385 <kiranbaraiya0308@gmail.com>
2025-03-21 14:14:17 +0530

    Updated

--- project/src/pages/Events.tsx
+++ project/src/pages/Events.tsx
@@ -213,6 +213,8 @@ export default function Events() {
                 className="pl-10 pr-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 w-64"
               />
             </div>
+
+            
             {(userRole === 'alumni' || userRole === 'faculty') && isLoggedIn && !editingEvent && (
               <motion.button
                 onClick={() => setShowAddForm(!showAddForm)}
